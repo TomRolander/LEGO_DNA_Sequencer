fa757684a26d7763e79629328fc654896acda92a
Tom Rolander <rolander@stanford.edu>
2022-12-24 13:12:37 -0800

    Create LEGO_DNA_Sequencer.ino

---
+++ LEGO_DNA_Sequencer.ino
@@ -0,0 +1,31 @@
+/**************************************************************************
+  LEGO DNA Sequencer
+  Main Program
+  
+  Original Code:  2022-12-24
+
+  Tom Rolander, MSEE
+  Mentor for Circuit Design, Software, 3D Printing
+  Miller Library, Fabrication Lab
+  Hopkins Marine Station, Stanford University,
+  120 Ocean View Blvd, Pacific Grove, CA 93950
+  +1 831.915.9526 | rolander@stanford.edu
+
+ **************************************************************************/
+
+#define PROGRAM "LEGO DNA Sequencer"
+#define VERSION "Ver 0.1 2022-12-24"
+
+#define DEBUG_OUTPUT 1
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
